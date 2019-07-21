87bf9a39b32422d8e8695cc59421fd17aa21a92b
leiwenchao <52775825+leiwenchao161718@users.noreply.github.com>
2019-07-21 21:56:30 +0800

    旅游攻略页面
    lwc

---
+++ index.vue
@@ -0,0 +1,404 @@
+<template>
+    <div class="container">
+        <div class="main">
+            <div class="el-row is-justify-space-between el-row--flex">
+                <div class="menus-wrapper">
+                    <div class="menus-body">
+                        <div class="menus">
+                            <div class="menu-item">
+                                <span>热门城市
+                                    <i class="el-icon-arrow-right"></i>
+                                </span>
+                                
+                            </div>
+                            <div class="menu-item">
+                                <span>推荐城市
+                                    <i class="el-icon-arrow-right"></i>
+                                </span>
+                                
+                            </div>
+                            <div class="menu-item">
+                                <span>奔向海岛
+                                    <i class="el-icon-arrow-right"></i>
+                                </span>
+                                
+                            </div>
+                            <div class="menu-item">
+                                <span>主题推荐
+                                    <i class="el-icon-arrow-right"></i>
+                                </span>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="aside-recommend">
+                        <h5 class="aside-title">推荐城市</h5>
+                        <a href="" class="aside-recommend-item">
+                        <img src="http://157.122.54.189:9093/images/pic_sea.jpeg"></a>
+                    </div>
+                </div>
+                <div class="post-wrapper">
+                    <div class="search-wrapper">
+                        <div class="search-bar el-row is-justify-space-between is-align-middle el-row--flex">
+                           <el-input placeholder="请输入想去的地方,比如说:'广州'"  class="input-with-select">
+                           <el-button slot="append" icon="el-icon-search"></el-button>
+                           </el-input>
+                        </div>
+                        <div class="search-recommend">
+                            推荐:
+                            <span>广州</span>
+                            <span>上海</span>
+                            <span>北京</span>
+                        </div>
+                    </div>
+                    <div class="post-title el-row is-justify-space-between is-align-middle el-row--flex">
+                        <h4>推荐攻略</h4>
+                        <button type="button" class="el-button el-button-primary">
+                            <i class="el-icon-edit"></i>
+                            <span>写游记</span>
+                        </button>
+                        
+                    </div>
+                    <div class="post-list">
+                        <div class="post-item card">
+                            <h4 title="塞班贵?一定是你打开的方式不对!6000块玩转塞班">
+                                    塞班贵?一定是你打开的方式不对!6000块玩转塞班
+                            </h4>
+                            <p>
+                                <a href="">
+                                    大家对塞班岛总存在着这样的误解，
+                                    知道它是美属地盘，就理所当然地觉得这里的花费一定很高，
+                                    花费高有高的玩法，那如果只有6000块的预算呢？要怎么玩？
+                                    关于旅行这件事，我们要让钱花得更有道理，收下这份攻略，
+                                    带你6000块花式玩转塞班。图：塞班岛。
+                                     by第5季旅游一、怎样用6000块玩转塞班？
+                                     大多数出境游客人不做预算或最终花费远远超出预算，
+                                     对预算的合理分配对控制我们旅行的花费就很重要了，
+                                     如何只花6000块玩转塞班岛，对预算超支say&nbsp;no？
+                                     下面从5个方面来为您一一解读：
+                                     机票+酒店、岛上交通、玩乐项目、吃以及购买纪念品。
+                                     1.怎样订机票+酒店性价比最高？
+                                     机票和酒店的花销往往会占据我们旅行大半的花销，
+                                     假设机票酒店为塞班行预算的一半，剩下的吃行玩购为预算的另一半，
+                                     如果能在机票酒店这部分省下钱，
+                                     也就意味着在塞班岛用来吃行玩购的钱就增加了怎样去塞班？
+                                     可以转机也可以直飞，转机大多会从韩国转，
+                                     提前蹲守能买到韩国飞塞班的特价机票，2000以下就能入手，
+                                     加上国内飞韩国的机票来回塞班得5000+，还没算上在塞班的住宿费用，
+                                     转机还有中途等待的时间，光花在路途上的时间就比直飞要多上一倍甚至更多，
+                                     转乘奔波劳累，非联程票还要担心行李托运问题，所以建议大家有直飞还是尽量选择直飞。
+                                     在酒店上，旅途中我们呆在酒店的时间远比在外游玩的时间少，
+                                     酒店干净整洁基本就能满足我们休息的需求，塞班不是个享受酒店的地方而且还真不能跟国内星级酒店等位比较，
+                                     所以不建议大家花过多的钱在塞班的酒店体验上。
+                                     怎样在机票酒店上获得最高性价比的体验？&nbsp;直飞塞班的航班一般和酒店一起打包成机票+酒店套餐，
+                                     价格要比单定机票、酒店要更加便捷实惠，往往3千多就能把机票和酒店一键搞定.
+                                     </a>
+                                     </p>
+                                     <div class="card-images el-row is-justify-space-between is-align-middle el-row--flex">
+                                         <a href="">
+                                         <img src="https://n3-q.mafengwo.net/s10/M00/E8/E4/wKgBZ1octoCABhgLAAafahORRLs91.jpeg?imageView2%2F2%2Fw%2F1360%2Fq%2F90">
+                                         </a>
+                                         <a href="">
+                                         <img src="https://images.mafengwo.net/images/i/face/brands_v3/6@2x.png">
+                                         </a>
+                                         <a href="">
+                                         <img src="https://p1-q.mafengwo.net/s10/M00/E9/33/wKgBZ1octwiAAKAoAAJ9ixcJc9M71.jpeg?imageView2%2F2%2Fw%2F1360%2Fq%2F90">
+                                         </a>
+                                     </div>
+                                     <div class="post-info el-row is-justify-space-between el-row--flex">
+                                            <div class="post-info-left el-row is-align-middle el-row--flex">
+                                                    <span>
+                                                        <i class="el-icon-location-outline"></i>
+                                                        北京市
+                                                    </span>
+                                                    <div class="post-user el-row is-align-middle el-row--flex">
+                                                        <p>by</p>
+                                                        <a href="">
+                                                            <img src="http://157.122.54.189:9095/assets/images/avatar.jpg">
+                                                        </a>
+                                                        <a href="">地球发动机</a>
+                                                    </div>
+                                                    <span>
+                                                        <i class="el-icon-view">3221</i>
+                                                    </span>
+                                            </div>
+                                            <span class="post-info-right">31赞</span>
+                                     </div>
+                        </div>
+                            <div  data-v-053600ae="">
+                                <div class="post-item image-text el-row is-justify-space-between is-align-middle el-row--flex">
+                                    <div class="post-cover el-row is-align-middle">
+                                        <a href="" class="">
+                                            <img src="https://p3-q.mafengwo.net/s13/M00/41/C4/wKgEaVyaOs2AA9IKAAj8Lg2YzaU64.jpeg?imageMogr2%2Fthumbnail%2F1360x%2Fstrip%2Fquality%2F90">
+                                        </a>
+                                    </div> 
+                                    <div class="post-content">
+                                        <h4 title="远东行：用好奇心打量这座城 —— 最值得收藏的海参崴出行攻略">
+                                            <a href="">
+                                              远东行：用好奇心打量这座城 —— 最值得收藏的海参崴出行攻略
+                                            </a>
+                                        </h4> 
+                       <p>
+                       <a href="">
+                        想象一下一个距离&nbsp;北京&nbsp;只有2.5小时飞行距离的城市：
+                        身处&nbsp;亚洲&nbsp;却能感受到十足的欧陆风情——欧式建筑和街道，
+                        金发碧眼的路人，正宗的西餐外加只有国内一半售价的帝王蟹可以敞开吃——更难能可贵的是，
+                        这里对国人（实质）免签，有直飞航班，低廉的物价，
+                        且尚未有太多的游客涉足还保留着原汁原味的传统风情！
+                        </a>
+                        </p> 
+                        <div class="post-info el-row is-justify-space-between el-row--flex">
+                            <div class="post-info-left el-row is-align-middle el-row--flex">
+                                <span>
+                                    <i class="el-icon-location-outline"></i>
+                                    青岛市
+                                </span> 
+                                <div class="post-user el-row is-align-middle el-row--flex">
+                                 by 
+                            <a href="">
+                              <img src="http://157.122.54.189:9095/assets/images/avatar.jpg">
+                            </a> 
+                            <a href="">
+                            地球发动机
+                            </a>
+                        </div>
+                         <span>
+                             <i class="el-icon-view"></i> 
+                             540
+                        </span>
+                        </div> 
+                        <span class="post-info-right">4 赞</span>
+                        </div>
+                        </div>
+                        </div> 
+                        </div>
+                        <div class="post-item card">
+                            <h4 title="这里是成都! 一座来了就不想走的城市">这里是成都! 一座来了就不想走的城市</h4>
+                            <p>
+                                <a href="">
+                                    时间：2019年4月30日～2019年5月4日人物：
+                                    我们一家三口地点：&nbsp;成都&nbsp;设备：
+                                    7M2+FE55F1.8行李：一个28寸行李箱熊娃现在是小学二年级下学期了，
+                                    今年她听完了凯叔三国演义，最喜欢刘备的蜀国，
+                                    会嫌弃阿斗太笨是被摔傻了……于是在五一假期从1天变成4天的之后，
+                                    我订了来回&nbsp;成都&nbsp;的机票，
+                                    决定带她来看一看刘备的蜀国～四川&nbsp;很大，
+                                    可以玩的地方很多，但是只有4天，
+                                    开始还安排了去&nbsp;青城山&nbsp;和&nbsp;都江堰&nbsp;，
+                                    后面决定还是市区走走吃吃就好了～出发前行程行前准备：
+                                    1、机票机票是深航里程兑换，没太多可以说的。
+                                    2、酒店成都&nbsp;的酒店很多，各种价位总能找到你心水的。
+                                    我们这次主要游玩的是市区，除了最远的大熊猫基地要十几公里外，
+                                    其他都集中在二环以内。我们出行主要的交通工具是滴滴，
+                                    所以考虑要找个在这些比较中心的地方住会比较方便。
+                                    一开始想住在春熙路，方便逛，看了好几家网红酒店。
+                                    后面我哥跟我说飞猪F2每年有两次住宿权益让我关注下，
+                                    那天正好周四，明宇还有五折，最后就选了明宇旗下的尚雅酒店订了下来，
+                                    酒店位置很好，距离各大景区都在三四公里内，订下来尚雅客房才354一晚，
+                                    性价比超高！不过飞猪只能定两晚，剩下的一晚也是支付宝活动，缤客288返现88，
+                                    订了一晚最低价的旅行者客房426.87，再返现88，起来是338.87，
+                                    然后入住的时候不出意外滴升级到和前两晚一样的房型了，省得换房！
+                                    住下来感觉性价比超高的！最后一晚，因为4号是早上八点多的飞机，
+                                    我们不愿意早起，于是就跑机场旁边去住了，也是三百多～
+                                    3、门票成都&nbsp;的景点门票都不贵，线上线下也都很方便，
+                                    而且几乎没便宜多少，有的还会更贵，&nbsp;比如&nbsp;武侯祠门票50，
+                                    线上卖到了五十几，还要门口排队取票，我果断退款了线上票，
+                                    门口直接买了，还快了不少。更有大熊猫繁育研究基地是打车路上提前买的，
+                                    结果后面大门口的时候，电子票爆满，实体票&nbsp;通道&nbsp;一个人都没有，
+                                    我内心是崩溃的～所以真心不着急线上买，到时候为了省线上几块钱，
+                                    多排半个小时您觉着划算吗？唯一行前定下的就是芙蓉国粹的票，
+                                    飞猪上用里程抵扣后3张甲等票509.99，位置超好！这个可以有！
+                                    PS：不过线不线上买还是具体看地方，杜甫草堂的时候，
+                                    线上还是有优势的～只能说具体看了4、保险依然是买个保险傍身，
+                                    只是国内游没太多要求，随意买就行了～5、主要用用到的APP飞猪、
+                                    携程、大众点评、百度地图、滴滴PS：我们比较懒，懒的走，
+                                    地铁虽然方便但是有一种叫走死在到地铁站的路上，
+                                    所以一般国内城市出行我都选尽量近的住宿靠滴滴，
+                                    实在需要地铁的城市选择住地铁站附近～这次团了两个滴滴五一的套餐，
+                                    正好覆盖了五一出行的四天第一天 4.30 我们出发啦！宽窄巷子你泉州晋江机场我们是下午一点半的飞机，
+                                    于是给熊娃请了假一放学就赶飞机去了。
+                                    从&nbsp;泉州&nbsp;到&nbsp;成都&nbsp;要三个小时，
+                                    所以到&nbsp;成都&nbsp;已然饭点了。本来想好了第一餐是想吃&nbsp;成都&nbsp;吃客的，
+                                    于是下了飞机就在手机上排了号，结果30那天大堵车平时二十分钟能到的酒店活生生走了四五十分钟，
+                                    而排号也在我们快到酒店前过号了。酒店位置不错，除了宽窄巷子，其他景点都在二十分钟车程以内，
+                                    这样方便我们回酒店午休～带孩子出门吃饭和睡觉是最重要的成都明宇尚雅饭店在酒店放下行李，
+                                    就准备到宽窄巷子吃饭，结果很尴尬的事发生了，竟然叫不到车。最后只能门口扫了两辆摩拜骑过去了，
+                                    还好也不远，骑车宽窄巷子二十几分钟。宽窄巷子景区宽窄巷子分为宽巷子（巷子比较宽），
+                                    窄巷子（巷子比较窄），井巷子（巷子里有口井），是清朝旗人的居住地，所以和&nbsp;北京&nbsp;的巷子文化有异曲同工之妙。
+                                    现在的宽窄巷子多为翻建的建筑，里面遍布了不少有意思的店铺，如果人不多，不赶时间可以好好逛逛。我们主要走了窄巷子，
+                                    人潮那个汹涌，所以没有时间慢慢逛。宽巷子和井巷子也稍微逛了一下，好像还是窄巷子人最多。
+                                </a>
+                            </p>
+                            <div class="card-images el-row is-justify-space-between is-align-middle el-row--flex">
+                                <a href="">
+                                    <img src="http://n4-q.mafengwo.net/s13/M00/B7/94/wKgEaVzLgBeAAEP6AAQP-fApUus99.jpeg?imageMogr2%2Fthumbnail%2F1360x%2Fstrip%2Fquality%2F90" >
+                                </a>
+                                <a href="">
+                                    <img src="http://b3-q.mafengwo.net/s13/M00/FF/47/wKgEaVzMTOCAHX9_AAnidFWU9K466.jpeg?imageMogr2%2Fthumbnail%2F1360x%2Fstrip%2Fquality%2F90" >
+                                </a>
+                                <a href="">
+                                    <img src="http://p4-q.mafengwo.net/s13/M00/02/39/wKgEaVzMTgKALFi5AAaKsyezLBE34.jpeg?imageMogr2%2Fthumbnail%2F1360x%2Fstrip%2Fquality%2F90">
+                                </a>
+                            </div>
+                            <div class="post-info el-row is-justify-space-between el-row--flex">
+                                            <div class="post-info-left el-row is-align-middle el-row--flex">
+                                                    <span>
+                                                        <i class="el-icon-location-outline"></i>
+                                                        成都市
+                                                    </span>
+                                                    <div class="post-user el-row is-align-middle el-row--flex">
+                                                        <p>by</p>
+                                                        <a href="">
+                                                            <img src="http://157.122.54.189:9095/assets/images/avatar.jpg">
+                                                        </a>
+                                                        <a href="">地球发动机</a>
+                                                    </div>
+                                                    <span>
+                                                        <i class="el-icon-view">439</i>
+                                                    </span>
+                                            </div>
+                                            <span class="post-info-right">10赞</span>
+                                     </div>
+                           </div>
+                        </div>
+                    </div>
+            </div>
+            <div el-pagination>
+              <span class="el-pagination_total">共100页</span>
+            </div>
+        </div>
+    
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+.container {
+  padding: 30px 0;
+}
+.main {
+  width: 1000px;
+  margin: 0 auto;
+}
+.main .menus {
+  width: 260px;
+  border: 1px solid #ddd;
+}
+.menus .menu-item {
+  height: 50px;
+  line-height: 50px;
+  font-size: 16px;
+  border: #ddd 1px solid;
+}
+.menus .menu-item > span {
+  margin-left: 24px;
+}
+.menus .menu-item i {
+  margin-left: 140px;
+}
+.aside-recommend {
+  margin-top: 30px;
+}
+.aside-recommend .aside-title {
+  color: #000;
+  font-weight: 400;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #ddd;
+  margin-bottom: 10px;
+}
+.aside-recommend img {
+  width: 260px;
+  height: 173px;
+}
+.post-wrapper {
+  margin-left: 50px;
+}
+.search-wrapper {
+  width: 800px;
+  margin-left: 50px;
+}
+.search-wrapper input {
+  width: 500px;
+  height: 42px;
+  line-height: 42px;
+}
+.search-wrapper button {
+  color: orange;
+  text-align: center;
+}
+.search-wrapper .search-bar {
+  color: orange;
+  border: 2px solid orange;
+}
+.search-recommend > span {
+  font-size: 14px;
+}
+.post-title {
+  margin-top: 30px;
+}
+.post-title h4 {
+  font-weight: 400;
+  font-size: 18px;
+  color: orange;
+  position: relative;
+  margin-left: 50px;
+}
+.post-title > button {
+  width: 130px;
+  height: 50px;
+  color: #fff;
+  background-color: #409eff;
+}
+.post-title {
+  width: 100%;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #eee;
+  position: relative;
+}
+.post-list {
+  margin-left: 50px;
+  margin-top: 30px;
+}
+.post-list .post-item > h4 {
+  font-size: 20px;
+  font-weight: normal;
+}
+.post-item p > a {
+  font-size: 14px;
+}
+.post-item .card-images img {
+  width: 220px;
+  height: 150px;
+  padding-top: 24px;
+}
+.post-item .post-cover img {
+  width: 220px;
+  height: 150px;
+}
+.post-info .post-info-left {
+  font-size: 12px;
+  color: #ccc;
+  margin: 12px;
+}
+.post-info .post-info-left .post-user a {
+  padding: 12px;
+}
+.post-info .post-user > p {
+  margin-left: 12px;
+}
+.post-info .post-user img {
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+}
+.post-info .post-info-right {
+  color: orange;
+}
+</style>
